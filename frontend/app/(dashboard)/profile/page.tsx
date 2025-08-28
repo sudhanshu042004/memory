f7aa2b0a7f50650cb5c786ce2242fd3f51627d00
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2025-08-28 09:56:46 +0530

    added profile page && change stack screen header style

--- frontend/app/(dashboard)/profile/page.tsx
+++ frontend/app/(dashboard)/profile/page.tsx
@@ -1,11 +1,33 @@
-import { View,Text } from "react-native"
+import { Inter_400Regular, Inter_500Medium, Inter_700Bold, useFonts } from "@expo-google-fonts/inter";
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
 
-const profilePage=()=>{
-    return(
+
+const profilePage = () => {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_700Bold,
+        Inter_500Medium,
+      });
+    return (
         <>
-        <View>
-            <Text>This is Profile page</Text>
-        </View>
+            <View>
+                <Stack.Screen options={{
+                    headerShown: true, headerStyle: {
+                        backgroundColor: "black",
+                    },
+                    headerTitleStyle: {
+                        color: "white",
+                        fontFamily: "Inter_500Medium",
+                        fontSize: 19,
+                        fontWeight: "500",
+                    },
+                    headerTintColor : "white",
+                    title: "Profile",
+                    animation: "slide_from_right"
+                }} />
+                <Text>This is Profile page</Text>
+            </View>
         </>
     )
 }
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <StatusBar style='light' />
+      <StatusBar style='inverted' />
       <Stack screenOptions={{ headerShown: false }} />
     </AuthProvider>
   );
