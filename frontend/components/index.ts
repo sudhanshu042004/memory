ae4ea3b59d43cc9f09deaefb7fb889858f4573ac
harash <harash.poriya@gmail.com>
2025-09-03 18:22:50 +0530

    All components export

---
+++ frontend/components/index.ts
@@ -0,0 +1,8 @@
+export { default as HeaderButtons } from './HeaderButtons';
+export { default as GreetingSection } from './GreetingSection';
+export { default as QuickActions } from './QuickActions';
+export { default as StatsSection } from './StatsOverview';
+export { default as MemoryCategories } from './MemoryCategories';
+export { default as RecentActivity } from './RecentActivity';
+export { default as QuickTips } from './QuickActions';
+export { default as FloatingAddMenu } from './FloatingAddMenu';
